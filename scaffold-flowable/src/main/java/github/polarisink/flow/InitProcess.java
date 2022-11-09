d3ba744706127de267022430963f58550cc7005b
aries <polarisink@163.com>
2022-11-09 11:14:28 +0800

    remove passwd

--- scaffold-flowable/src/main/java/github/polarisink/flow/InitProcess.java
+++ scaffold-flowable/src/main/java/github/polarisink/flow/InitProcess.java
@@ -12,9 +12,7 @@ public class InitProcess {
   public static void main(String[] args) {
     // 流程引擎配置
     ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
-        .setJdbcUrl("jdbc:mysql://1.13.169.163:3306/flow?autoRec&useUnicode=true&characterEncoding=utf8&serverTimezone=Asia/Shanghai&useSSL=false")
         .setJdbcUsername("root")
-        .setJdbcPassword("vgy87dgy16rhd61fAGHbcgA")
         .setJdbcDriver("com.mysql.cj.jdbc.Driver")
         // 初始化基础表，不需要的可以改为 DB_SCHEMA_UPDATE_FALSE
         .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_FALSE);
