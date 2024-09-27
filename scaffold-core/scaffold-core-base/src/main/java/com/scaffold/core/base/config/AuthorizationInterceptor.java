d35ff770d686f4d40d58e8fc79548d6b61ca4f21
aries <polarisink@163.com>
2024-09-27 10:23:33 +0800

    增加远程调用配置

---
+++ scaffold-core/scaffold-core-base/src/main/java/com/scaffold/core/base/config/AuthorizationInterceptor.java
@@ -0,0 +1,22 @@
+package com.scaffold.core.base.config;
+
+import com.scaffold.core.base.util.ServletUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class AuthorizationInterceptor implements ClientHttpRequestInterceptor {
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        request.getHeaders().add(HttpHeaders.AUTHORIZATION, ServletUtils.getHeader(HttpHeaders.AUTHORIZATION));
+        return execution.execute(request, body);
+    }
+}
