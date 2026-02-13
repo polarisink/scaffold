86ec5ea8ee994ee069fa906d68dc91abd7126728
aries <polarisink@163.com>
2026-02-13 10:54:01 +0800

    测试sb-admin

--- scaffold-test/scaffold-test-langchain4j/src/main/java/com/scaffold/LangChain4jApplication.java
+++ scaffold-test/scaffold-test-langchain4j/src/main/java/com/scaffold/LangChain4jApplication.java
@@ -1,9 +1,12 @@
 package com.scaffold;
 
+import cn.hutool.extra.spring.EnableSpringUtil;
+import dev.langchain4j.spring.LangChain4jAutoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@EnableSpringUtil
+@SpringBootApplication(exclude = LangChain4jAutoConfig.class)
 public class LangChain4jApplication {
     public static void main(String[] args) {
         SpringApplication.run(LangChain4jApplication.class, args);
---
+++ scaffold-test/scaffold-test-sb-admin-client/src/main/java/com/polarisink/admin/SbAdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.polarisink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbAdminClientApplication.class, args);
+    }
+
+}
